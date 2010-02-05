bf1e4e77ff5e174d2c4032603ed4e514fffc1d7a
Carl Sverre <carl@rain.(none)>
2010-02-05 01:55:17 -0800

    Lots of changes, mostly trying to get a feel...
    ...for ruby and this program.

--- todd.rb
+++ todd.rb
@@ -1,13 +1,92 @@
 require 'todd_model'
+require 'digest/md5'
+
+config = {
+  :default_db_path  =>  '/home/carl/dev/todd/test.db',
+  :config_filename  =>  '.todd',
+  :default_config   =>  <<-eos
+conf = {
+  :todd_hash      =>  '%s'
+}
+eos
+}
+
+module Docs
+  Short = {
+    :init  => ["todd init        ", "Initialize a Todd list in the local directory."],
+    :add   => ["todd add <string>", "Add an item to the local Todd list."],
+    :rm    => ["todd rm <id>     ", "Remove an item from the local todo list."],
+    :list  => ["todd list        ", "List all items in all categories."],
+    :find  => ["todd find <query>", "List all items which match the query."],
+    :start => ["todd start <id>  ", "Start the timer on task with id == <id>."],
+    :stop  => ["todd stop <id>   ", "Stop the timer on task with id == <id>."]
+  }
+end
+
+def docs_for(method)
+  puts "%s\t%s" % Docs::Short[method.to_sym]
+end
 
 class Todd
+
+  @config = {}
+
   def initialize(config)
-    @todd_hash = config[:list_hash]
+    @config = config
   end
 
   # COMMANDS
   
   def init
+    current_dir = Dir.getwd
+    current_dir_md5 = Digest::MD5.hexdigest(current_dir)
+
+    File.open(@config[:config_filename], 'w') do |f|
+      f.write(sprintf(@config[:default_config],current_dir_md5))
+    end
+
+    p "Initialized Todd in #{current_dir}"
+  end
+
+  def add task_str
+    category_name = "default"
+    task_str = task_str.sub(/#(\S*)/) do |cat|
+      puts "Cat name: #{$1}"
+      category_name = $1
+      ""
+    end
+    task_str.strip!
+
+    puts "Task: #{task_str}"
+
+    begin
+      category = Category.get!(:name => category_name)
+    rescue DataMapper::ObjectNotFoundError => e
+      category = Category.new(:name => category_name)
+      category.save
+    end
+
+    task = category.tasks.new(:title => task_str)
+    task.save
+  end
+
+  def rm
+  end
+
+  def list
+    Task.all.each do |task|
+      puts "%d\t%s\t%s" % [task.id, task.title, task.category.name]
+    end
+  end
+
+  def find
+  end
+
+  def start
+  end
+
+  def stop id
+    p id
   end
 
   def add_remote
@@ -17,41 +96,39 @@ class Todd
   def sync
     p "Not Implemented"
   end
-
-  def add
-  end
-
-  def rm
-  end
-
-  def list
-  end
-
-  def find
-  end
-
-  def start
-  end
-
-  # stop <id>
-  def stop id
-    p id
-  end
 end
 
 # Parse the .todd file if it exists
-# Adds the Conf hash to the global namespace for now
+conf = nil
 begin
-  eval file.new(".todd").read
+  eval File.open(config[:config_filename], 'r').read
+  config = conf.merge(config) unless conf == nil
 rescue ScriptError=>e
-  warn("An error occured while reading ./.todd",e)
+  warn("Error reading #{config_filename}, you must run todd init to use todd")
 end
 
-return if Conf.nil?
+todd = Todd.new(config)
 
-todd = Todd.new(Conf)
+setup_db config[:default_db_path]
 
-ARGV.each do |a|
-  puts "Argument: #{a}"
-  todd.method(a.intern).call()
+if ARGV.length > 0
+  command = ARGV.shift
+
+  break if command == "help"
+
+  begin
+    todd.send(command.to_sym, *ARGV)
+  rescue ArgumentError => e
+    docs_for command
+  end
+
+  exit
+end
+
+## show help
+
+puts "Usage: todd [OPTION] [ARGS]"
+
+Docs::Short.keys.each do |command|
+  docs_for key
 end
--- todd_model.rb
+++ todd_model.rb
@@ -1,28 +1,43 @@
 require 'rubygems'
 require 'dm-core'
+require 'dm-validations'
+require 'dm-timestamps'
 
 # Setup DataMapper
 
-DataMapper.setup(:default, 'sqlite3::memory:')
-
+def setup_db path = '/home/carl/dev/todd/test.db', adapter = 'sqlite3'
+  DataMapper.setup(:default, adapter + '://' + path)
+  begin
+    File::Stat.new(path)
+  rescue 
+    DataMapper.auto_migrate!
+  end
+end
 
 class Category
   include DataMapper::Resource
 
   property :id,           Serial
-  property :name,         String
-  property :created_at,   DateTime,   :default => DateTime.new
+  property :name,         String,     :required => true
+  property :created_at,   DateTime
+  property :updated_at,   DateTime
+
+  has n, :tasks
+
+  validates_is_unique :name
 end
 
 class Task
   include DataMapper::Resource
 
   property :id,           Serial
-  property :title,        String
-  property :notes,        Text
-  property :created_at,   DateTime,   :default => DateTime.new
-  property :deleted,      Boolean,    :default => false
-  property :deleted_at,   DateTime,   :default => DateTime.new
+  property :title,        String,     :required => true
+  property :notes,        Text,       :default => ""
+  property :created_at,   DateTime
+  property :updated_at,   DateTime
+  property :deleted,      ParanoidBoolean
+  property :deleted_at,   ParanoidDateTime
   
   belongs_to :category
 end
+
